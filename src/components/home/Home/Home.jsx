a433a0b846359df91d9a57b33d5501444b23289c
Bajlar01~ <bajlarrahman24@gmail.com>
2023-05-18 09:57:44 +0600

    react router setup

---
+++ src/components/home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='max-w-6xl mx-auto bg-red-400'>
+      <h2>This is a home</h2>
+    </div>
+  );
+};
+
+export default Home;
